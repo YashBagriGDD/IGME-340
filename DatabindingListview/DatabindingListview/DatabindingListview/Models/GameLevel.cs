ea9bda49bd5661a5f1e2677521b10546d75c4594
Yash Bagri <ybagri@gmail.com>
2020-09-20 22:23:26 -0400

    Finished Databindings HW

---
+++ DatabindingListview/DatabindingListview/DatabindingListview/Models/GameLevel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DatabindingListView.Models {
+    class GameLevel {
+        public string Name { get; set; }
+        public int LevelNumber { get; set; }
+        public int MinPlayerLevel { get; set; }
+        public int MaxPlayerLevel { get; set; }
+        public int Score { get; set; }
+        public int Deaths { get; set; }
+    }
+}
---
+++ DatabindingListview/DatabindingListview/DatabindingListview/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,39 @@
+﻿using DatabindingListView.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Reflection;
+using System.Text;
+using Xamarin.Forms;
+
+namespace DatabindingListView.ViewModels {
+    class MainPageViewModel {
+        public ObservableCollection<GameLevel> Levels { get; set; }
+        public Command AddCommand { get; }
+        public Command RemoveCommand { get; }
+
+        public MainPageViewModel() {
+            Levels = new ObservableCollection<GameLevel> {
+                new GameLevel {Name="Jyon", LevelNumber=1, MinPlayerLevel=1, MaxPlayerLevel=100, Score=500, Deaths=2},
+                new GameLevel {Name="Gamer420", LevelNumber=99, MinPlayerLevel=50, MaxPlayerLevel=200, Deaths=0, Score=9999999},
+                new GameLevel {Name="Serra", LevelNumber=6, MinPlayerLevel=1, MaxPlayerLevel=200, Score=34500, Deaths=5}
+            };
+
+            AddCommand = new Command(Add);
+            RemoveCommand = new Command(Remove);
+        }
+
+        private void Add() {
+            Levels.Add(new GameLevel { LevelNumber = Levels.Count, Name = "added level " + Levels.Count + 1});
+        }
+
+        private void Remove() {
+            if (Levels.Count >= 1) {
+                Levels.RemoveAt(Levels.Count - 1);
+            }
+            else {
+                // Prevents crash when at 0 levels.
+            }
+        }
+    }
+}
